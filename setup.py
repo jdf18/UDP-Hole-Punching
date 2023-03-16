75df27342e63bad286430cba4e563b57c5e80bbb
Jacob Flint <jacob.flint@outlook.com>
2023-03-16 22:27:50 +0000

    updated name to UDPHolePunching as can not use hyphens

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name="UDP-Hole-Punching",
+	url="https://github.com/jdf18/UDP-Hole-Punching",
+	author="Jacob Flint",
+	author_email="jacobd.flint@icloud.com",
+	packages=["UDPHolePunching"],
+	install_requires=[],
+	version="0.1",
+	licence="GPL-3.0",
+	description="An implementation of UDP Hole Punching using the python programming language.",
+	long_description=open("README.md").read(),
+)
